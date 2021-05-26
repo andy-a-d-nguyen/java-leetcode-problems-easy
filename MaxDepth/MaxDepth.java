2661084ea80ed9c55e498a12c767a3207a2f1251
andy-a-d-nguyen <a.d.nguyen.andy@gmail.com>
2021-05-25 21:19:43 -0700

    maxDepth complete

---
+++ MaxDepth/MaxDepth.java
@@ -0,0 +1,48 @@
+/*
+    A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+
+    It is an empty string "", or a single character not equal to "(" or ")",
+    It can be written as AB (A concatenated with B), where A and B are VPS's, or
+    It can be written as (A), where A is a VPS.
+    We can similarly define the nesting depth depth(S) of any VPS S as follows:
+
+    depth("") = 0
+    depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+    depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+    depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+    For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+
+    Given a VPS represented as string s, return the nesting depth of s.
+
+    Input: string
+    Output: int
+    Constraints: 1 <= s.length <= 100
+                 s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+                 It is guaranteed that parentheses expression s is a VPS.
+*/
+
+public class MaxDepth {
+    public static int maxDepth(String s) {
+        
+        int depth = 0;
+        int currentDepth = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // because s is a guaranteed VPS, a left parenthesis will always have an accompanying right parenthesis
+            // therefore, we can find the max depth by keeping a total tally of the left parenthesis
+            if (s.charAt(i) == '(') {
+                currentDepth++;
+                depth = Math.max(depth, currentDepth);
+            } else if (s.charAt(i) == ')') {
+                currentDepth--;
+            }
+        }
+
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(maxDepth("(1+(2*3)+((8)/4))+1"));
+        System.out.println(maxDepth("(1)+((2))+(((3)))"));
+    }
+}
