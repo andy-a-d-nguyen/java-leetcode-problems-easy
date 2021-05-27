b80f225a8423726ac7e3aad3d5adc6fd4a16cb2a
andy-a-d-nguyen <a.d.nguyen.andy@gmail.com>
2021-05-26 20:22:13 -0700

    arrayStringsAreEqual complete

---
+++ ArrayStringsAreEqual/ArrayStringsAreEqual.java
@@ -0,0 +1,72 @@
+/*
+    Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+    A string is represented by an array if the array elements concatenated in order forms the string.
+
+    Input: 2 string arrays
+    Output: boolean
+    Constraints: 1 <= word1.length, word2.length <= 103
+                 1 <= word1[i].length, word2[i].length <= 103
+                 1 <= sum(word1[i].length), sum(word2[i].length) <= 103
+                 word1[i] and word2[i] consist of lowercase letters.
+*/
+
+public class ArrayStringsAreEqual {
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder stringBuilder1 = new StringBuilder();
+        StringBuilder stringBuilder2 = new StringBuilder();
+
+        for (String word : word1) {
+            stringBuilder1.append(word);
+        }
+
+        for (String word : word2) {
+            stringBuilder2.append(word);
+        }
+
+        String string1 = stringBuilder1.toString();
+        String string2 = stringBuilder2.toString();
+
+        if (string1.equals(string2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arrayStringsAreEqual(new String[] {"a", "cb"}, new String[] {"ab", "c"}));
+        System.out.println(arrayStringsAreEqual(new String[] {"a", "bc"}, new String[] {"ab", "c"}));
+
+    }
+}
+
+/*
+    ALTERNATIVE SOLUTION:
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int p1 = 0, p2 = 0; // inner pointer
+        int w1 = 0, w2 = 0; // outer pointer
+
+        while (w1 < word1.length && w2 < word2.length) {
+            String currWord1 = word1[w1], currWord2 = word2[w2];
+
+            if (currWord1.charAt(p1) != currWord2.charAt(p2)) return false;
+
+            if (p1 < currWord1.length() - 1) {
+                p1++;
+            } else {
+                p1 = 0;
+                w1++;
+            }
+
+            if (p2 < currWord2.length() - 1) {
+                p2++;
+            } else {
+                p2 = 0;
+                w2++;
+            }
+        }
+
+        return w1 == word1.length && w2 == word2.length;
+    }
+*/
